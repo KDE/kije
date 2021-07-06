4979db596598fea88867101c70f50ed0dc50afb8
Jan Blackquill <uhhadd@gmail.com>
2021-07-06 16:58:53 -0400

    feat: qml runner linked against qtwidgets using QApplication

---
+++ examples/widgets-runner/main.cpp
@@ -0,0 +1,36 @@
+/*
+ *  SPDX-FileCopyrightText: 2020 Carson Black <uhhadd@gmail.com>
+ *
+ *  SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+ 
+#include <QApplication>
+#include <QCoreApplication>
+#include <QQmlApplicationEngine>
+#include <QUrl>
+
+int main(int argc, char* argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setApplicationName("org.kde.Tok");
+    QCoreApplication::setOrganizationName("KDE");
+
+    QApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+    const QUrl url(app.arguments()[1]);
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreated,
+        &app,
+        [url](QObject* obj, const QUrl& objUrl) {
+            if ((obj == nullptr) && url == objUrl) {
+                QCoreApplication::exit(-1);
+            }
+        },
+        Qt::QueuedConnection);
+    engine.load(url);
+
+    return QApplication::exec();
+}
